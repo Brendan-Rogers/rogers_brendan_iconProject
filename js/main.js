598f6d6a7745fbaf04867a2e129ca922f7d386de
Brendan Rogers <brendanr11@gmail.com>
2018-02-15 10:30:52 -0500

    Added js elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 (() => { 
 	console.log("connected");
+
+	var iconSet = document.querySelectorAll('svg');
+
+		function logThisIcon() {
+			console.log('clicked on this icon', this.id);
+
+			this.style.opacity = 0.5;
+		}
+
+
+	iconSet.forEach(icon => icon.addEventListener('click', logThisIcon));
 }) ();
